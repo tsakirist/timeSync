fcb04504b7721ce31ea1bb685e3ab8d2f789f79c
tsakirist <tsakiris@uth.gr>
2016-07-21 14:18:26 +0300

    Fixed some erros regarding seconds-milliseconds

--- client.js
+++ client.js
@@ -1,31 +1,43 @@
 const timesync = require('timesync');
+const exec = require('child_process').exec;
 
 const  ts = timesync.create({
     server: 'http://localhost:3000/timesync',
-    interval: 5000  // Set the sync interval to 5sec
 });
 
 // Get notified when sync starts/ends
 ts.on('sync', arg => {
+    if(arg === 'end') {
+        let now = ts.now();
+        /** Because set date command takes seconds as argument
+         * if we want to pass milliseconds we need to divide
+         * the number by 1000
+         */
+        now = now / 1000;
+        console.log("Now:", now);
+        setDate(now);
+    }
     console.log(`Synchronization ${arg === 'start' ? 'started' : 'ended' }`);
 });
 
 // Get notified on changes in the offset
 ts.on('change', offset => {
     console.log('Offset:',offset);
-    console.log('Sync time:',ts.now());
 });
 
-/** Execute below command to set the date
+/** Function setDate
+ *  executes below command to set the date
  *  sudo date --set @ <number>
- *  <number> is expressed in milliseconds
+ *  <number> is expressed in seconds
  *  (root privilege is required)
  */
-const exec = require('child_process').exec;
-exec('sudo date --set @772543821', (error, stdout, stderr) => {
-    if (error!=null) {
-        console.log(error);
-        return;
-    }
-    console.log('Changed the date to:\n', stdout);
-});
+function setDate(date) {
+    exec(`sudo date --set @${date}`, (error, stdout, stderr) => {
+        if (error!=null) {
+            console.log('Error:',error);
+            return;
+        }
+        console.log('Changed the date to:\n', stdout);
+    });
+}
+
