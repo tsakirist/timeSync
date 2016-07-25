b470dcedd2fc846aa841bddca5f5d3ef6a0707c9
tsakirist <tsakiris@uth.gr>
2016-07-25 14:11:57 +0300

    Client now works with promises, added a config.json file for available options

--- client.js
+++ client.js
@@ -1,43 +1,88 @@
+'use strict';
+const execFile = require('child_process').execFile;
 const timesync = require('timesync');
-const exec = require('child_process').exec;
 
-const  ts = timesync.create({
-    server: 'http://localhost:3000/timesync',
-});
+class Client {
 
-// Get notified when sync starts/ends
-ts.on('sync', arg => {
-    if(arg === 'end') {
-        let now = ts.now();
-        /** Because set date command takes seconds as argument
-         * if we want to pass milliseconds we need to divide
-         * the number by 1000
-         */
-        now = now / 1000;
-        console.log("Now:", now);
-        setDate(now);
+    constructor(configFile) {
+        this.config = require(configFile);
     }
-    console.log(`Synchronization ${arg === 'start' ? 'started' : 'ended' }`);
-});
 
-// Get notified on changes in the offset
-ts.on('change', offset => {
-    console.log('Offset:',offset);
-});
-
-/** Function setDate
- *  executes below command to set the date
- *  sudo date --set @ <number>
- *  <number> is expressed in seconds
- *  (root privilege is required)
- */
-function setDate(date) {
-    exec(`sudo date --set @${date}`, (error, stdout, stderr) => {
-        if (error!=null) {
-            console.log('Error:',error);
+    start() {
+        try {
+            this.tsClient = this.create();
+        }
+        catch (err) {
+            console.log(err);
             return;
         }
-        console.log('Changed the date to:\n', stdout);
-    });
+        this.tsClient.on('sync', arg => {
+            if(arg === 'end') {
+                let date = Math.floor(this.tsClient.now()/1000);
+                this.setDate(date).then((out) => {
+                    console.log("Resolved.\nChanged the date to:\n", out);
+                }, (err)=> {
+                    console.log('Rejected.\n', err);
+                });
+            }
+            console.log(`Synchronization ${arg === 'start' ? 'started' : 'ended' }`);
+        });
+    }
+
+    create() {
+        if(!this.config.server) {
+            throw new Error('You need to set the server URL in the config.json file.');
+        }
+        return timesync.create(this.config);
+    }
+
+    /** Function setDate
+     *  executes below command to set the date
+     *  date --set @ <number>
+     *  <number> is expressed in seconds
+     *  (root privilege is required)
+     *  @returns {Promise}
+     */
+    setDate(date) {
+        return new Promise((resolve, reject) => {
+            // exec(`date --set @${date}`, (error, stdout) => {
+            execFile('date', ['--set', `@${date}`], (error, stdout) => {
+                if (error!=null) {
+                    reject(error);
+                }
+                else {
+                    this.setHwClock().then(()=> {
+                        resolve(stdout);
+                    }, (error) => {
+                        reject(error);
+                    });
+                }
+            });
+        });
+    }
+
+    /** Function setHwClock
+     *  sets the hardware clock from the current system time
+     * @returns {Promise}
+     */
+    setHwClock() {
+        return new Promise((resolve, reject)=>{
+            execFile('hwclock', ['-w'], (error) => {
+                if(error) {
+                    reject(error);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+    }
 }
 
+/** config.json is the configuration file
+ *  make your changes there
+ */
+const configFile = "./config.json";
+
+const client = new Client(configFile);
+client.start();
